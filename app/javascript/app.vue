8e47bff3b9fe55bb98d79388275ab10b409369bb
H-hidetaka <sansanhidetaka@gmail.com>
2021-03-27 19:21:34 +0900

    [Modify]app.vue修正及びrspec追加

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,27 +1,32 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-    <button type=button>はじめる</button>
+  <div id='app' class="d-flex flex-column min-vh-100">
+    <header class="mb-auto">
+      <nav class="navbar navbar-dark bg-dark">
+        <span class="nacbar-brand mb-0 h1">{{ title }}</span>
+      </nav>
+    </header>
+  <div class="text-center">
+    <h3>タスクを管理しよう</h3>
+    <div class="mt-4">生活や仕事に関するタスクを見える化して抜け漏れを防ぎましょう。</div>
+    <button type=button class="btn btn-dark mt-5">はじめる</button>
+  </div>
+  <footer class="mt-auto text-center">
+    <small>Copyright &copy; 2020. RUNTEQ</small>
+  </footer>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  name: "Top",
+  data(){
     return {
-      message: "Hello Vue!"
+      title: "タスク管理アプリ"
     }
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-button{
-  font-size: 30px;
-  text-align: center;
-}
+
 </style>
