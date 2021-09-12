20231530ab8e07b3aba3ee0aa196059323f06dae
H-hidetaka <sansanhidetaka@gmail.com>
2021-09-12 14:59:20 +0900

    [Modify]コンポーネント切り分け

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <div class="d-flex flex-column min-vh-100">
+    <TheHeader class="mb-auto" />
     <router-view />
+    <TheFooter class="mt-auto" />
   </div>
 </template>
+
+<script>
+import TheHeader from 'components/TheHeader.vue'
+import TheFooter from 'components/TheFooter.vue'
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter
+  },
+  name: "TopIndex",
+  data(){
+    return {
+      title: "タスク管理アプリ"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ app/javascript/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="mb-auto">
+      <nav class="navbar navbar-dark bg-dark">
+        <span class="nacbar-brand mb-0 h1">{{ title }}</span>
+      </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+  title: "タスク管理アプリ"
+}
+</script>
+
+<style scoped>
+</style>
