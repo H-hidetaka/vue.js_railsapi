9473ff174b19b522040a2ca4a6e6dcd8ba4a39bc
H-hidetaka <sansanhidetaka@gmail.com>
2021-09-16 19:57:20 +0900

    [Modify]タスク詳細表示

--- app/javascript/pages/task/index.vue
+++ app/javascript/pages/task/index.vue
@@ -3,11 +3,20 @@
     <div class="d-flex">
       <div class="col-4 bg-light rounded shadow m-3 p-3">
         <div class="h4">TODO</div>
-        <div v-for="task in tasks" :key="task.id" class="bg-white border shadow-sm rounded my-2 p-4">
+        <div
+          v-for="task in tasks"
+          :key="task.id"
+          :id="'task-' + task.id"
+          class="bg-white border shadow-sm rounded my-2 p-4"
+          @click="handleShowTaskDetailModal(task)"
+        >
           <span>{{ task.title }}</span>
         </div>
       </div>
     </div>
+    <transition name="fade">
+      <TaskDetailModal v-if="isVisibleTaskDetailModal" :task="taskDetail" @close-modal="handleCloseTaskDetailModal" />
+    </transition>
     <div class="text-center">
       <router-link :to="{ name: 'TopIndex' }" class="btn btn-dark mt-5">戻る</router-link>
     </div>
@@ -15,26 +24,41 @@
 </template>
 
 <script>
+import TaskDetailModal from "./components/TaskDetailModal"
+
 export default {
   name: "TaskIndex",
-  data(){
+  components: {
+    TaskDetailModal
+  },
+  data() {
     return {
-      title: "タスク管理アプリ",
-      tasks: []
+      tasks: [],
+      taskDetail: {},
+      isVisibleTaskDetailModal: false
     }
   },
   created() {
-    this.fetchTasks();
-  },
-  methods: {
-    fetchTasks() {
       this.$axios.get("tasks")
-      .then(res => this.tasks = res.data)
-      .catch(err => console.log(err.status));
+        .then(res => this.tasks = res.data)
+        .catch(err => console.log(err.status));
+    },
+    handleShowTaskDetailModal(task) {
+      this.isVisibleTaskDetailModal = true;
+      this.taskDetail = task;
+    },
+    handleCloseTaskDetailModal() {
+      this.isVisibleTaskDetailModal = false;
+      this.taskDetail = {};
     }
-  }
 }
 </script>
 
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
